83a767f170e12d09eece9b5cca220a168c4ac9ac
yugogamer <57806524+yugogamer@users.noreply.github.com>
2022-02-01 15:44:21 +0100

    first iteration

---
+++ src/main.rs
@@ -0,0 +1,73 @@
+use tokio::{net::{TcpListener, TcpStream}, io::{AsyncReadExt, AsyncWriteExt}, sync::broadcast::{self, Sender}};
+
+
+#[derive(Clone)]
+enum Message {
+    Message(String)
+}
+
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").await.expect("Can't open server");
+    let (main_tx, mut _main_rx) = broadcast::channel::<Message>(16);
+    
+    loop {
+        match listener.accept().await {
+            Ok((socket, addr)) => {
+                let sender = main_tx.clone();
+                tokio::spawn(async move {
+                    println!("new connection from : {:?}", addr);
+                    process_socket(socket, sender).await;
+                });
+            },
+            Err(err) => eprintln!("Err connection refused : {}", err),
+        }
+    }
+}
+
+
+async fn process_socket(mut socket : TcpStream, sender : Sender<Message>){
+    let mut buf = [0; 1024];
+    
+    loop {
+        let sender = sender.clone();
+        let mut receiver = sender.subscribe();
+        tokio::select! {
+            
+            Ok(message_lenght) = socket.read(&mut buf) => {
+                if message_lenght == 0 {
+                    return
+                }
+                
+                let message_bin = buf.to_vec();
+                
+                let message_text = std::str::from_utf8(&message_bin);
+                
+                match message_text{
+                    Ok(message_text) => {
+                        let to_send = Message::Message(message_text.trim().to_owned());
+                        let result = sender.send(to_send);
+                        if result.is_err(){
+                            eprintln!("err, can't write in channel")
+                        }
+                    },
+                    Err(err) => {
+                        eprintln!("{}", err)
+                    }
+                }
+            }
+            
+            Ok(message_receive) = receiver.recv() => {
+                match message_receive{
+                    Message::Message(message) =>  {
+                        let result = socket.write_all(message.as_bytes()).await;
+                        if result.is_err(){
+                            eprintln!("err, can't write in socket")
+                        }
+                    },
+                }
+            }
+        }
+    }
+}
